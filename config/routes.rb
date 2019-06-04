bbae827d19ae4c9c2f0d5e9dd2f87ac63398e338
Yves Lesaffre <yves.lesaffre@gmail.com>
2019-06-04 17:59:57 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'orders/edit'
+  get 'orders/index'
+  get 'orders/show'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "home#index"
+  root to: 'pages#home'
+  resources :products
+  resources :orders
+
+  resources :addresses
+
+
+
+
 end
